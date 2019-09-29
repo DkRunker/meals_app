a2b92828f2f8299ab3477bdd9970131d0f35a843
Pablo Bahillo <bahillo.ortego.pablo@gmail.com>
2019-09-29 22:37:20 +0200

    Add TabBar & Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/filters_screen.dart';
+import './screens/tabs_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/meal_detail_screen.dart';
@@ -30,10 +32,12 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: MyHomePage(),
+      //home: MyHomePage(),
       routes: {
+        '/' : (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName : (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName : (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName : (ctx) => FiltersScreen(), 
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(builder: (ctx) => MyHomePage());
